1f39d07c8518d5e2d7dd34d062485fb927f87b78
Sean McDaniel <sm.fm.static@gmail.com>
2022-11-15 23:50:19 -0500

    Successfully set up env

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,2 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello world</h1>
+    )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+const element = <Index />;
+root.render(element);
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const stylesHandler = "style-loader";
 
 const config = {
-  entry: "./src/index.jsx",
+  entry: "./client/src/Index.jsx",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
