bc877e5128b93c81ef14b14906c1fb7fc4a4b2c2
Sean McDaniel <sm.fm.static@gmail.com>
2022-11-18 17:46:54 -0500

    pull request test 1

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './components/App.jsx';
 
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello world</h1>
+      // adding comment for test with pull request
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(<App />);
