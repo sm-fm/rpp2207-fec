3969aff4085d78a9fdb4b1ce7722c6c182e68f79
Calvin Goodale <calvin.goodale@gmail.com>
2022-11-18 18:12:43 -0500

    Added react-router-dom to App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Overview from './Overview.jsx';
 import Questions from './Questions.jsx';
 import Ratings from './Ratings.jsx';
@@ -7,13 +13,13 @@ import '../style.css';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <h1>App.jsx</h1>
       <Overview />
       <Questions />
       <Ratings />
       <Related />
-    </>
+    </Router>
   )
 }
 
