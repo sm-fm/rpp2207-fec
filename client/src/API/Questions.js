4f7e6f6c67a9d0e18b5a7d830e97c234a633cce5
bossertjack <82617739+bossertjack@users.noreply.github.com>
2022-11-19 14:42:56 -0700

    Added getAllProducts

--- client/src/API/Questions.js
+++ client/src/API/Questions.js
@@ -1,5 +1,21 @@
 const Questions = {
-
+  getAllQuestions: () => {
+    fetch('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/qa/questions', {
+      method: 'GET',
+      headers: {
+        'Authorization': GITHUB_ACCESS_TOKEN
+      }
+    })
+    .then(results => {
+      return results.json();
+    })
+    .then(results => {
+      console.log(results);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
 };
 
 export default Questions;
