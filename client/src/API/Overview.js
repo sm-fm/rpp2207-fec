7cd93b03d7b6bbe42343e93f7c8c5170c389a32d
Sean McDaniel <sm.fm.static@gmail.com>
2022-11-20 19:12:32 -0500

    Merged feature into test

--- client/src/API/Overview.js
+++ client/src/API/Overview.js
@@ -2,7 +2,7 @@ import GITHUB_ACCESS_TOKEN from '../auth.js';
 
 const Overview = {
   getAllProducts: () => {
-    fetch('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products', {
+    return fetch('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products', {
       method: 'GET',
       headers: {
         'Authorization': GITHUB_ACCESS_TOKEN
@@ -11,14 +11,25 @@ const Overview = {
       .then(results => {
         return results.json();
       })
-      .then(results => {
-        console.log(results);
-      })
       .catch(err => {
         console.log(err);
       });
   },
 
+  getProductById: (id) => {
+    return fetch(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': GITHUB_ACCESS_TOKEN
+      }
+    })
+      .then(result => {
+        return result.json();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 };
 
 export default Overview;
