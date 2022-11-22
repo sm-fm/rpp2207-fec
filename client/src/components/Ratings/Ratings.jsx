ba724883b4e2db435a477e57e921a39d35a3ed1e
Justin Stendara <jnstendara@gmail.com>
2022-11-21 21:15:08 -0600

    Initial commit of the ratings branch

--- client/src/components/Ratings/Ratings.jsx
+++ client/src/components/Ratings/Ratings.jsx
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 const Ratings = () => {
   return (
-    <h1>Ratings.jsx</h1>
+    <div className='ratings'>
+      <h1>Ratings.jsx</h1>
+    </div>
+
   )
 }
 
