3678efd8a5d2ef30cd767bcfbba26d46b3b8e807
Justin Stendara <jnstendara@gmail.com>
2022-11-22 03:55:55 -0600

    Added heading to the Ratings list

--- client/src/components/Ratings/Ratings.jsx
+++ client/src/components/Ratings/Ratings.jsx
@@ -5,7 +5,14 @@ const Ratings = (props) => {
   console.log(props.data);
   return (
     <div className='ratings'>
-
+      <div className='reviewListHeading'>
+        <label>{props.data.results.length} reviews, sorted by </label>
+        <select id='sortBy'>
+          <option value='relevance'>relevance</option>
+          <option value='newest'>newest</option>
+          <option value='helpful'>most helpful</option>
+        </select>
+      </div>
       {props.data.results.map((elem, idx) => {
         return (
         <UserReviews generateStars = {props.generateStars} key={`reviews-${idx}`} data={elem}/>
