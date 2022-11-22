95f76636dbe6077a23511047a80bf50c9fc0593a
Justin Stendara <jnstendara@gmail.com>
2022-11-22 03:07:53 -0600

    Have css added correctly for the user review cards

--- client/src/components/Ratings/ReviewCard.jsx
+++ client/src/components/Ratings/ReviewCard.jsx
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import format from 'date-fns/format';
+import parseISO from 'date-fns/parseISO';
 import './rating.css';
 
 let ReviewCard = (props) => {
-  console.log(props)
+  console.log(props.data.date)
+  var date = new Date(props.data.date);
+  console.log(date);
+  date = format(date, 'MMM d, y');
   return(
     <div className = 'userReview'>
-      {props.generateStars(props.data.rating, 'userReview')}
+      <div className='flex-box'>
+        <div className='starHolder'>
+          {props.generateStars(props.data.rating, 'userReview')}
+        </div>
+        <h6 id='username'>{`${props.data.reviewer_name}, ${date}`}</h6>
+      </div>
+      <h3 className='summary'>{props.data.summary}</h3>
+      <p className='reviewBody'>{props.data.body}</p>
+      {props.data.recommend &&
+        <p>✓ I recommend this product</p>}
+      {props.data.response !== '' &&
+        <p className='companyResponse'>{props.data.response}</p>}
+      <h6>Helpful? <u>Yes</u> {`(${props.data.helpfulness})`} | <u>Report</u></h6>
+
+      <div className='thumbnail-holder'>
+        {props.data.photos.map((element, idx) => {
+          return (
+            <img key={`${props.data.reviewer_name} image - ${idx + 1}`} className='thumbnail' src={element.url} alt={`${props.data.reviewer_name} image - ${idx + 1}`}/>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- client/src/components/Ratings/exampleData/exampleDataRatings.js
+++ client/src/components/Ratings/exampleData/exampleDataRatings.js
@@ -15,11 +15,11 @@ let example = {
       "helpfulness": 5,
       "photos": [{
           "id": 1,
-          "url": "urlplaceholder/review_5_photo_number_1.jpg"
+          "url": "https://assets.hermes.com/is/image/hermesproduct/freestyle-sneaker--222920ZH90-worn-1-0-0-800-800_b.jpg"
         },
         {
           "id": 2,
-          "url": "urlplaceholder/review_5_photo_number_2.jpg"
+          "url": "https://assets.hermes.com/is/image/hermesproduct/freestyle-sneaker--222920ZH90-worn-2-0-0-800-800_b.jpg"
         },
         // ...
       ]
@@ -28,7 +28,7 @@ let example = {
       "review_id": 3,
       "rating": 4,
       "summary": "I am liking these glasses",
-      "recommend": false,
+      "recommend": true,
       "response": "Glad you're enjoying the product!",
       "body": "They are very dark. But that's good because I'm in very sunny spots",
       "date": "2019-06-23T00:00:00.000Z",
