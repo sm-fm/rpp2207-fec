dfbab0d78ad6dddf906df4fa6f8f877caf0902af
Calvin Goodale <calvin.goodale@gmail.com>
2022-11-22 19:08:00 -0500

    Finished API call for Related Products + styles.

--- client/src/components/Related/ProductCard.jsx
+++ client/src/components/Related/ProductCard.jsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react';
 const RelatedProduct = (props) => {
   return (
     <div className='product-card-container'>
-      <div className='product-card-image'>
-        <div className='product-card-action-btn'></div>
-      </div>
+      <img className='product-card-image' src={props.product.styles.results[0].photos[0].thumbnail_url}>
+      </img>
+      <div className='product-card-action-btn'></div>
       <div className='product-card-description'>
         <div className='product-card-category'>{props.product.category}</div>
         <div className='product-card-name'>{props.product.name}</div>
