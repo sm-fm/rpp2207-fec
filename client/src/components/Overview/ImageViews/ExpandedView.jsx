9f111c7518cb56d8ce8baaf0b0a7e20e646afce4
Sean McDaniel <sm.fm.static@gmail.com>
2022-11-25 16:52:42 -0500

    Implemented icons to indicate the number of photos in a particular style that are available that dynamically transform based on which image in the list is being viewed

--- client/src/components/Overview/ImageViews/ExpandedView.jsx
+++ client/src/components/Overview/ImageViews/ExpandedView.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { v4 as uuidv4 } from 'uuid';
 
 const ExpandedView = (props) => {
 
@@ -14,6 +16,7 @@ const ExpandedView = (props) => {
   const handleRightClick = () => {
     setIndexOfCurrentImg(indexOfCurrentImg + 1);
   }
+
   return (
     <div id="expanded-view">
       {indexOfCurrentImg === 0
@@ -24,6 +27,14 @@ const ExpandedView = (props) => {
           ? null
           : <FontAwesomeIcon id="right-arrow" icon={faAngleRight} onClick={() => handleRightClick()} />
           }
+      <div id="circle-expanded-view">
+        {props.photos.map((photo, index) => {
+          const highlight = {
+            transform: indexOfCurrentImg === index ? 'scale(2.5)' : null
+          };
+          return <FontAwesomeIcon id="circle" style={highlight} icon={faCircle} key={uuidv4()} />
+        })}
+      </div>
       <img id="expanded-img" src={props.chosenStyle.photos[indexOfCurrentImg].thumbnail_url} />
     </div>
   );
