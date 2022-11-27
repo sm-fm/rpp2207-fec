b24ad9287f0665ccb55b60fb27c0697882a666d7
Calvin Goodale <calvin.goodale@gmail.com>
2022-11-27 18:42:15 -0500

    Added client.related.test.js file for Related Products testing

---
+++ testing/client.related.test.js
@@ -0,0 +1,83 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {cleanup, fireEvent, render, screen} from '@testing-library/react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useSearchParams
+} from "react-router-dom";
+import App from '../client/src/components/App.jsx';
+import Related from '../client/src/components/Related/Related.jsx';
+import RelatedProducts from '../client/src/components/Related/RelatedProducts.jsx';
+import YourOutfit from '../client/src/components/Related/YourOutfit.jsx';
+import ProductCard from '../client/src/components/Related/ProductCard.jsx';
+const fetch = require('node-fetch');
+globalThis.fetch = fetch
+global.window = Object.create(window);
+const url = "http://127.0.0.1:3000/71698";
+Object.defineProperty(window, 'location', {
+  value: {
+    href: url
+  }
+});
+describe("RelatedProducts component", () => {
+  test("Test 1", async () => {
+    render(<App />)
+    const items = await screen.findAllByText("Morning Joggers")
+    expect(items).toHaveLength(1);
+
+  });
+  test("Test 2", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 3", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 4", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 5", () => {
+    expect(true).toBeTrue;
+  });
+})
+
+describe("YourOutfit component", () => {
+  test("Test 1", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 2", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 3", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 4", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 5", () => {
+    expect(true).toBeTrue;
+  });
+})
+
+describe("ProductCard component", () => {
+  test("Test 1", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 2", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 3", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 4", () => {
+    expect(true).toBeTrue;
+  });
+  test("Test 5", () => {
+    expect(true).toBeTrue;
+  });
+})
