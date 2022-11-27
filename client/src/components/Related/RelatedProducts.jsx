a49bb140d99721c8023cecb6210b68951d983789
Calvin Goodale <calvin.goodale@gmail.com>
2022-11-27 10:59:56 -0500

    Added carousel to YourOutfit.jsx component

--- client/src/components/Related/RelatedProducts.jsx
+++ client/src/components/Related/RelatedProducts.jsx
@@ -4,8 +4,9 @@ import ProductCard from './ProductCard.jsx';
 
 
 const RelatedProducts = (props) => {
-  const [position, setPosition] = useState(1);
+  const [position, setPosition] = useState(0);
   const componentName = "RelatedProducts";
+
   return (
     <>
       <div className='related-products-container' style={{marginLeft: `-${position}px`}}>
@@ -28,13 +29,19 @@ const RelatedProducts = (props) => {
         }
 
       </div>
-      <div className='fade'>
+      <div className='fade-top'>
+        { position > 0 ?
         <div className="arrow-container-left">
           <div className="arrow-left" onClick={() => {setPosition(position - 250)}}></div>
         </div>
+        : null
+        }
+        { props && props.relatedProducts && position <= (props.relatedProducts.length - 4) * 250?
         <div className="arrow-container-right">
           <div className="arrow-right" onClick={() => {setPosition(position + 250)}}></div>
         </div>
+        : null
+        }
       </div>
     </>
   )
--- client/src/components/Related/YourOutfit.jsx
+++ client/src/components/Related/YourOutfit.jsx
@@ -2,24 +2,42 @@ import React, { useState, useEffect } from 'react';
 import ProductCard from './ProductCard.jsx';
 
 const YourOutfit = (props) => {
+  const [position, setPosition] = useState(0);
   const componentName = 'YourOutfit';
+
   return (
-    <div className='your-outfit-container'>
-      {props.yourOutfit ?
-      props.yourOutfit.map((product) => {
-        return <ProductCard
-        key={product.id}
-        product={product}
-        generateStars={props.generateStars}
-        isFetching={props.isFetching}
-        setIsFetching={props.setIsFetching}
-        parentComponent={componentName}
-        removeFromOutfit={props.removeFromOutfit}
-        />
-      })
-      : null
-      }
-    </div>
+    <>
+      <div className='your-outfit-container' style={{marginLeft: `-${position}px`}}>
+        {props.yourOutfit ?
+        props.yourOutfit.map((product) => {
+          return <ProductCard
+          key={product.id}
+          product={product}
+          generateStars={props.generateStars}
+          isFetching={props.isFetching}
+          setIsFetching={props.setIsFetching}
+          parentComponent={componentName}
+          removeFromOutfit={props.removeFromOutfit}
+          />
+        })
+        : null
+        }
+      </div>
+      <div className='fade-bottom'>
+        { position > 0 ?
+        <div className="arrow-container-left">
+          <div className="arrow-left" onClick={() => {setPosition(position - 250)}}></div>
+        </div>
+        : null
+        }
+        { props && props.yourOutfit && position <= (props.yourOutfit.length - 4) * 250?
+        <div className="arrow-container-right">
+          <div className="arrow-right" onClick={() => {setPosition(position + 250)}}></div>
+        </div>
+        : null
+        }
+      </div>
+    </>
   )
 }
 
