33568d75568d1a9dbe9a183d9049dbdd088351e3
Calvin Goodale <calvin.goodale@gmail.com>
2022-11-27 15:50:37 -0500

    Added identity-obj-proxy to package.json for handling css in jest

--- testing/client.hfs.test.js
+++
@@ -1,6 +0,0 @@
-let hf = require('../client/src/components/helperFunctions.js').helperFunctions
-test('make sure the query HF works', ()=> {
-  const testURL = 'http://localhost:3000/1234'
-  const expectedID = '1234';
-  expect(hf.getIDFromURL(testURL)).toBe(expectedID)
-})
