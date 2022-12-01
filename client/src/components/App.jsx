471270bb1c0be90732554bf6def8e9ed79f37dc5
Calvin Goodale <calvin.goodale@gmail.com>
2022-11-30 19:17:03 -0500

    Moved Router to index.jsx and changed id in App.jsx to useParams()

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,7 @@ import {
   Switch,
   Route,
   Link,
-  useSearchParams
+  useParams
 } from "react-router-dom";
 import Overview from './Overview/Overview.jsx';
 import Questions from './Questions/Questions.jsx';
@@ -14,20 +14,21 @@ import '../style.css';
 let helperFunctions = require('./helperFunctions.js').helperFunctions;
 
 const App = () => {
-  let id = helperFunctions.getIDFromURL(window.location.href);
+  const params = useParams();
+  const id = params.id;
   const [yourOutfit, setYourOutfit] = useState([]);
   const addToOutfit = (product) => {
     setYourOutfit(yourOutfit => ([...yourOutfit, product]));
   }
 
   return (
-    <Router>
+    <>
       <h1>App.jsx</h1>
       <Overview objID={id} yourOutfit={yourOutfit} addToOutfit={addToOutfit}/>
       <Questions objID={id}/>
       <Ratings objID={id}/>
       <Related objID={id} yourOutfit={yourOutfit} addToOutfit={addToOutfit}/>
-    </Router>
+    </>
   )
 }
 
