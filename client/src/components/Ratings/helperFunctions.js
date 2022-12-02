5204d2472f5c3237b407f27e087d187889549a61
Justin Stendara <jnstendara@gmail.com>
2022-12-02 16:22:44 -0600

    Removed tests from the helper functions file

--- client/src/components/Ratings/helperFunctions.js
+++ client/src/components/Ratings/helperFunctions.js
@@ -27,47 +27,6 @@ let printResults = (expected, actual, testName) => {
   }
 }
 
-let calculateRecommended = (rec) => {
-  if (typeof (rec) !== 'object') {
-    return null;
-  }
-  return (parseInt(rec.true) / parseInt(rec.true) + parseInt(rec.false)).toFixed(0)
-}
-
-let calculateAverageReviewsTests = () => {
-  let expected, actual, testName;
-
-  expected = null;
-  actual = calculateAverageReviews(undefined);
-  testName = 'Test 1 - Should return null for non-object type inputted';
-  printResults(expected, actual, testName);
-
-  expected = 3.8;
-  actual = calculateAverageReviews({1: '20', 2: '19', 3: '38', 4: '43', 5: '86'});
-  testName = 'Test 2 - Should return average rating rounded to the nearest tenth';
-  printResults(expected, actual, testName);
-
-  expected = 2;
-  actual = calculateAverageReviews({1: '0', 2: '20', 3: '0', 4: '0', 5: '0'});
-  testName = 'Test 3 - If only one rating has votes it should return that rating';
-  printResults(expected, actual, testName);
-}
-
-let calculateRecommendedTests = () => {
-  let expected, actual, testName;
-
-  expected = null;
-  actual = calculateRecommended(undefined);
-  testName = 'Test 1 - Should return null for non-object type inputted';
-  printResults(expected, actual, testName);
-
-  expected = 55;
-  actual = calculateRecommended({false:'54', true:'152'});
-  testName = 'Test 2 - Should return a whole number when given clean data';
-  printResults(expected, actual, testName);
-}
-// calculateAverageReviewsTests();
-// calculateRecommendedTests();
 module.exports = {
   calculateAverageReviews: calculateAverageReviews,
   calculateRecommended: calculateRecommended
