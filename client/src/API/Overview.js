7f8aaa4dcc89117b4847b2db57bf73b9e1b0900c
Calvin Goodale <calvin.goodale@gmail.com>
2022-12-01 20:45:20 -0500

    Removed refs to auth.js and changed API urls

--- client/src/API/Overview.js
+++ client/src/API/Overview.js
@@ -1,14 +1,9 @@
-import GITHUB_ACCESS_TOKEN from '../auth.js';
+
 const fetch = require('node-fetch');
 
 const Overview = {
   getAllProducts: () => {
-    return fetch('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products', {
-      method: 'GET',
-      headers: {
-        'Authorization': GITHUB_ACCESS_TOKEN
-      }
-    })
+    return fetch('products')
       .then(results => {
         return results.json();
       })
@@ -18,12 +13,7 @@ const Overview = {
   },
 
   getProductById: (id) => {
-    return fetch(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': GITHUB_ACCESS_TOKEN
-      }
-    })
+    return fetch(`products/${id}`)
       .then(result => {
         return result.json();
       })
