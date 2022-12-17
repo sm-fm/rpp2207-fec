9f64d1300e85117f42f44fd372631d22d68f2892
Justin Stendara <jnstendara@gmail.com>
2022-12-17 12:56:20 -0600

    Added icon to modal

--- client/src/components/Ratings/modal.jsx
+++ client/src/components/Ratings/modal.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faXmark} from '@fortawesome/free-solid-svg-icons';
 
 let Modal = (props) => {
   console.log('modal is loading in');
   return (
     <div className='reviews-modal' onBlur={props.onClick}>
       <div className='image-wrapper'>
-        <span id='reviews-modal-overlay' onClick={props.onClick}>x</span>
+        <span id='reviews-modal-overlay' onClick={props.onClick}><FontAwesomeIcon icon={faXmark} /></span>
         <img src={props.src}
           alt='Sorry, the image could not be loaded at this time, please try again later.'/>
       </div>
