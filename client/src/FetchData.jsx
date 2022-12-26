f5ec1698424b0b13fdec60af7fdb30d4efe658b5
Sean McDaniel <sm.fm.static@gmail.com>
2022-12-26 12:48:26 -0500

    Refactored FetchData to be a fat arrow function to satisfy linter

--- client/src/FetchData.jsx
+++ client/src/FetchData.jsx
@@ -4,7 +4,7 @@ import QuestionsAPI from './API/Questions.js';
 import RatingsAPI from './API/Ratings.js';
 import RelatedAPI from './API/Related.js';
 
-function FetchData(WrappedComponent, id) {
+const FetchData = (WrappedComponent, id) => {
 
   class WithData extends React.Component {
     constructor(props) {
@@ -32,85 +32,85 @@ function FetchData(WrappedComponent, id) {
       };
     }
 
-      componentDidMount() {
-        this.setState({ id });
-        OverviewAPI.getProductById(id)
-          .then(result => {
-            return result.json();
-          })
-          .then(result => {
-            this.setState({product: result});
-            return RatingsAPI.getReviewMetadata(result.id);
-          })
-          .then(metadata => {
-            this.setState({avgRatings: OverviewAPI.getAverageRating(metadata.ratings)});
-            return OverviewAPI.getStylesById(metadata.product_id);
-          })
-          .then(styles => {
-            return styles.json();
-          })
-          .then(styles => {
-            this.setState({
-              styles: styles.results,
-              chosenStyle: styles.results[0],
-              photos: styles.results[0].photos,
-              toggleClick: styles.results[0].name,
-              skus: styles.results[0].skus
-            });
-            return OverviewAPI.getAllReviews(styles.product_id);
-          })
-          .then(reviews => {
-            this.setState({ reviews });
-            return RatingsAPI.getReviewList(reviews.product, 'relevant', 1, 100);
-          })
-          .then(data => {
-            this.setState({
-              allData: data
-            });
-            return RatingsAPI.getAll(data.product);
-          })
-          .then(data => {
-            this.setState({
-              metaData: data[1],
-              reviewData: data[0],
-              allData: data[0]
-            });
-            return QuestionsAPI.getAllQuestions(data[0].product);
-          })
-          .then(results => {
-            this.setState({
-              data: results,
-              questions: results
-            });
-            return RelatedAPI.getRelatedProducts(id);
-          })
-          .then(products => {
-            var productsMap = new Map();
-            products.forEach(product => productsMap.set(product.id, product));
-            this.setState({
-              relatedProducts: [...productsMap.values()]
-            });
-          })
-          .then(() => {
-            this.setState({fetching: false});
-          })
-          .catch(err => {
-            console.log(err);
-            this.setState({error: true});
+    componentDidMount() {
+      this.setState({ id });
+      OverviewAPI.getProductById(id)
+        .then(result => {
+          return result.json();
+        })
+        .then(result => {
+          this.setState({product: result});
+          return RatingsAPI.getReviewMetadata(result.id);
+        })
+        .then(metadata => {
+          this.setState({avgRatings: OverviewAPI.getAverageRating(metadata.ratings)});
+          return OverviewAPI.getStylesById(metadata.product_id);
+        })
+        .then(styles => {
+          return styles.json();
+        })
+        .then(styles => {
+          this.setState({
+            styles: styles.results,
+            chosenStyle: styles.results[0],
+            photos: styles.results[0].photos,
+            toggleClick: styles.results[0].name,
+            skus: styles.results[0].skus
           });
-      }
+          return OverviewAPI.getAllReviews(styles.product_id);
+        })
+        .then(reviews => {
+          this.setState({ reviews });
+          return RatingsAPI.getReviewList(reviews.product, 'relevant', 1, 100);
+        })
+        .then(data => {
+          this.setState({
+            allData: data
+          });
+          return RatingsAPI.getAll(data.product);
+        })
+        .then(data => {
+          this.setState({
+            metaData: data[1],
+            reviewData: data[0],
+            allData: data[0]
+          });
+          return QuestionsAPI.getAllQuestions(data[0].product);
+        })
+        .then(results => {
+          this.setState({
+            data: results,
+            questions: results
+          });
+          return RelatedAPI.getRelatedProducts(id);
+        })
+        .then(products => {
+          var productsMap = new Map();
+          products.forEach(product => productsMap.set(product.id, product));
+          this.setState({
+            relatedProducts: [...productsMap.values()]
+          });
+        })
+        .then(() => {
+          this.setState({fetching: false});
+        })
+        .catch(err => {
+          console.log(err);
+          this.setState({error: true});
+        });
+    }
 
 
-      render() {
-        if (this.state.error) {
-          return <p>There was an error fetching the data</p>
-        }
-        if (!this.state.fetching) {
-          return <WrappedComponent data={this.state} {...this.props} />
-        } else {
-          return <p>Loading...</p>
-        }
+    render() {
+      if (this.state.error) {
+        return <p>There was an error fetching the data</p>;
       }
+      if (!this.state.fetching) {
+        return <WrappedComponent data={this.state} {...this.props} />;
+      } else {
+        return <p>Loading...</p>;
+      }
+    }
   }
   return WithData;
 };
